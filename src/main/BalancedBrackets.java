a4c08cbcdb9609d75b782a23be62ce8937bf2487
mmille014 <miguel_miller@cable.comcast.com>
2020-05-01 15:14:50 -0400

    Unit testing

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -24,10 +24,11 @@ public class BalancedBrackets {
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
         for (char ch : str.toCharArray()) {
-            if (ch == '[') {
+            if (ch == ';') {
                 brackets++;
             } else if (ch == ']') {
                 brackets--;
+
             }
         }
         return brackets == 0;
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -8,9 +8,23 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
-    }
+    public void emptyTest() {assertEquals(true, true);}
+
+    @Test
+    public void commaexiststest() {assertEquals(true, true);}
+
+    @Test
+    public void semicolontest() {assertEquals(true, true);}
+
+    @Test
+    public void brackettest() {assertEquals(true, true);}
+
+
+
+
+
+
+
 
 
 }
